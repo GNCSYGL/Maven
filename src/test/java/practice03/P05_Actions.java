9c31b147a04220d3515d5d82624ad61679615f1b
Gonca Saygili <goncasaygili@gmail.com>
2023-01-17 23:54:19 +0300

    practice

--- src/test/java/practice03/P05_Actions.java
+++ src/test/java/practice03/P05_Actions.java
@@ -8,18 +8,30 @@ import utilities.TestBase;
 
 public class P05_Actions extends TestBase {
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
         // http://szimek.github.io/signature_pad/ sayfasına gidiniz
         driver.get("http://szimek.github.io/signature_pad/");
 
         // Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
-        WebElement yaziTahtasi = driver.findElement(By.xpath("//*[@style='touch-action: none; user-select: none;']"));
+        WebElement yaziTahtasi = driver.findElement(By.xpath("//canvas"));
         Actions actions = new Actions(driver);
-        actions.moveToElement(yaziTahtasi);
+        actions.moveToElement(yaziTahtasi).clickAndHold();
         for (int i=1; i<10; i++) {
-            actions.sendKeys("=").perform();
+            actions.moveByOffset(5, 5);
         }
+        for (int i=1; i<10; i++) {
+            actions.moveByOffset(5, 5);
+        }
+        for (int i=1; i<10; i++) {
+            actions.moveByOffset(5, 5);
+        }
+        actions.release().build().perform();
+
+        Thread.sleep(5000);
+
         // Çizimden sonra clear butonuna basalım
+        driver.findElement(By.xpath("//*[text()='Clear']")).click();
+
         // Son olarak sayfayı temizleyiniz
 
 
